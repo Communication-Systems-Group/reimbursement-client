893c42c5a29039e92afbceeed95b797cfe07b243
Robin <mail@xorox.ch>
2015-05-26 01:17:16 +0200

    Formatting

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,13 @@
 	"use strict";
 
 	deferredBootstrapper.bootstrap({
-		element : document.body,
-		module : 'reimbursement',
-		resolve : {
-			LANGUAGES : ['$http',
-			function($http) {
-				return $http.get('./languages/languages.json');
-			}]
+		element: document.body,
+		module: 'reimbursement',
+		resolve: {
+			LANGUAGES: ['$http',
+				function ($http) {
+					return $http.get('./languages/languages.json');
+				}]
 
 		}
 	});
@@ -19,52 +19,52 @@ var app = angular.module('reimbursement', ['reimbursement.templates', 'ui.router
 app.constant("Modernizr", Modernizr);
 
 app.config(['$stateProvider', '$urlRouterProvider', '$translateProvider', '$locationProvider', 'LANGUAGES',
-function($stateProvider, $urlRouterProvider, $translateProvider, $locationProvider, LANGUAGES) {
-	"use strict";
+	function ($stateProvider, $urlRouterProvider, $translateProvider, $locationProvider, LANGUAGES) {
+		"use strict";
 
-	for (var key in LANGUAGES) {
-		if (LANGUAGES.hasOwnProperty(key)) {
-			$translateProvider.translations(key, LANGUAGES[key]);
+		for (var key in LANGUAGES) {
+			if (LANGUAGES.hasOwnProperty(key)) {
+				$translateProvider.translations(key, LANGUAGES[key]);
+			}
 		}
-	}
-	$translateProvider.preferredLanguage('en');
+		$translateProvider.preferredLanguage('en');
 
-	$stateProvider.state('login', {
-		url : "/login",
-		templateUrl : "login/login.tpl.html",
-		controller : 'LoginController'
+		$stateProvider.state('login', {
+			url: "/login",
+			templateUrl: "login/login.tpl.html",
+			controller: 'LoginController'
 
-	}).state('signature', {
-		url : "/signature",
-		templateUrl : "signature/signature.tpl.html",
-		controller : "SignatureController"
+		}).state('signature', {
+					url: "/signature",
+					templateUrl: "signature/signature.tpl.html",
+					controller: "SignatureController"
 
-	}).state('cropping', {
-		url : "/cropping",
-		params : {
-			imageUri : null
-		},
-		templateUrl : "cropping/cropping.tpl.html",
-		controller : "CroppingController"
+				}).state('cropping', {
+					url: "/cropping",
+					params: {
+						imageUri: null
+					},
+					templateUrl: "cropping/cropping.tpl.html",
+					controller: "CroppingController"
 
-	}).state('dashboard', {
-		url : "/dashboard",
-		templateUrl : "dashboard/dashboard.tpl.html",
-		controller : "DashboardController"
+				}).state('dashboard', {
+					url: "/dashboard",
+					templateUrl: "dashboard/dashboard.tpl.html",
+					controller: "DashboardController"
 
-	}).state('expense', {
-		url : "/expense/:id",
-		templateUrl : "expense/expense.tpl.html",
-		controller : "ExpenseController"
+				}).state('expense', {
+					url: "/expense/:id",
+					templateUrl: "expense/expense.tpl.html",
+					controller: "ExpenseController"
 
-	}).state('csrfTestingPage', {
-		url : "/csrfTestingPage",
-		templateUrl : "csrfTestingPage/csrfTestingPage.tpl.html",
-		controller : 'CsrfTestingPageController'
-	});
+				}).state('csrfTestingPage', {
+					url: "/csrfTestingPage",
+					templateUrl: "csrfTestingPage/csrfTestingPage.tpl.html",
+					controller: 'CsrfTestingPageController'
+				});
 
-	$urlRouterProvider.otherwise('/signature');
+		$urlRouterProvider.otherwise('/signature');
 
-	$locationProvider.hashPrefix("!");
+		$locationProvider.hashPrefix("!");
 
-}]);
+	}]);
--- src/js/expense/expense.controller.js
+++ src/js/expense/expense.controller.js
@@ -1,327 +1,336 @@
 /**
  * Created by robinengbersen on 23.05.15.
  */
-<<<<<<< HEAD
-app.controller('ExpenseController', ['$scope', '$filter', '$state', '$stateParams', '$modal', function($scope, $filter, $state, $stateParams, $modal) {
-    "use strict";
-=======
 app.controller('ExpenseController', ['$scope', '$filter', '$state', '$stateParams',
->>>>>>> 27b1a18acbaa4d5b77a4c153e28b4e329e94a047
 
-function($scope, $filter, $state, $stateParams) {
-	"use strict";
 
-<<<<<<< HEAD
-    function find(obj,id) {
-        for(var i=0; i<obj.length; i++) {
-            if(obj[i].id === id) {
-                return [i,obj[i]];
-            }
-        }
-    }
+	function ($scope, $filter, $state, $stateParams) {
+		"use strict";
 
-    function getTotal() {
-        var total = 0;
-
-        for(var i=0; i<$scope.expense.receipts.length; i++) {
-            total += parseFloat($scope.expense.receipts[i].amount);
-        }
-
-        $scope.total = total;
-    }
-
-    $scope.alert = {
-        info: {
-            state: true,
-            value: $filter('translate')('reimbursement.expense.info')
-        },
-        success: {
-            state: false,
-            value: 'blabla'
-        },
-        danger: {
-            state: false,
-            value: 'blabla'
-        }
-    };
-
-    // ToDo this needs to be sent from the server
-    $scope.expense = {
-        id: 1,
-        creator: {
-            ldap_id: 23,
-            name: 'Jens Meier'
-        },
-        contact: {
-            person: {
-                ldap_id: 3,
-                name: 'Claudia Leibundgut'
-            },
-            phone: '044 786 23 45'
-        },
-        accounting: 'Reise nach New York',
-        note: [{
-            date: '2015-03-11T18:00:00.000+02:00',
-            person: {
-                ldap_id: 20,
-                name: 'Burkhard Stiller'
-            },
-            text: 'Es fehlen noch die Belege für das Abendessen im Fridays!'
-        },{
-            date: '2015-03-12T16:30:00.000+02:00',
-            person: {
-                ldap_id: 23,
-                name: 'Jens Meier'
-            },
-            text: 'Habe die Belege hinzugefügt'
-        }],
-        receipts: [{
-            id: 1,
-            date_receipt: '2015-01-13T18:00:00.000+02:00',
-            account: '310050',
-            description: 'Schwimmen im Hudson',
-            amount: {
-                amount_original: '150.75',
-                amount_currency: 'USD',
-                exchange_rate: '1.01',
-                amount: '155.80'
-            },
-            cost_center: 'E-10000-01-01'
-        },{
-            id: 2,
-            date_receipt: '2015-01-10T18:00:00.000+02:00',
-            account: '329100',
-            description: 'Flight ZRH => JFK',
-            amount: {
-                amount_original: '150.75',
-                amount_currency: 'USD',
-                exchange_rate: '1.01',
-                amount: '155.80'
-            },
-            cost_center: 'E-10000-01-01'
-        },{
-            id: 3,
-            date_receipt: '2015-01-14T18:00:00.000+02:00',
-            account: '329100',
-            description: 'Flight JFK => ZRH',
-            amount: {
-                amount_original: '150.75',
-                amount_currency: 'USD',
-                exchange_rate: '1.01',
-                amount: '155.80'
-            },
-            cost_center: 'E-10000-01-01'
-        },{
-            id: 4,
-            date_receipt: '2015-01-14T19:00:00.000+02:00',
-            account: '313020',
-            description: 'Zeichenblock',
-            amount: {
-                amount_original: '150.75',
-                amount_currency: 'USD',
-                exchange_rate: '1.01',
-                amount: '155.80'
-            },
-            cost_center: 'E-10000-01-01'
-        }],
-        documents: [{
-            url: 'url_to_pdf_doc',
-            belongs_to_receipt: [2,3]
-        },{
-            url: 'url_to_pdf_doc',
-            belongs_to_receipt: [1]
-        }]
-    };
-
-    getTotal();
-
-    $scope.uploadReceipt = function() {
-        // ToDo
-    };
-
-    $scope.addNewReceipt = function() {
-        $scope.receipt = {
-            id: 0,
-            date_receipt: null,
-            account: null,
-            description: '',
-            amount: {
-                original: '',
-                currency: 'CHF',
-                exchange_rate: '1.00',
-                value: ''
-            },
-            cost_center: {
-                prefix: 'E-1000',
-                value: ''
-            }
-        };
-
-        var modalInstance = $modal.open({
-            templateUrl : 'expense/receipt.tpl.html',
-            controller : 'ReceiptController',
-            scope: $scope
-        });
-
-        modalInstance.result.then(function(response) {
-            console.log(response);
-        });
-    };
-
-    $scope.documentExistsForExpense = function(receipt_id) {
-        for(var i=0; i<$scope.expense.documents.length; i++) {
-            if($scope.expense.documents[i].belongs_to_receipt.indexOf(receipt_id) !== -1) {
-                return true;
-            }
-        }
-        return false;
-    };
-=======
-	$scope.expenseId = $stateParams.id;
-	$scope.receiptChanges = false;
-
-	$scope.$watch(function() {
-		console.log("digest called");
-	});
-
-	function find(obj, id) {
-		for (var i = 0; i < obj.length; i++) {
-			if (obj[i].id === id) {
-				return [i, obj[i]];
+		function find(obj, id) {
+			for (var i = 0; i < obj.length; i++) {
+				if (obj[i].id === id) {
+					return [i, obj[i]];
+				}
 			}
 		}
-	}
 
-	$scope.alert = {
-		info : {
-			state : true,
-			value : $filter('translate')('reimbursement.expense.info')
-		},
-		success : {
-			state : false,
-			value : 'blabla'
-		},
-		danger : {
-			state : false,
-			value : 'blabla'
+		function getTotal() {
+			var total = 0;
+
+			for (var i = 0; i < $scope.expense.receipts.length; i++) {
+				total += parseFloat($scope.expense.receipts[i].amount);
+			}
+
+			$scope.total = total;
 		}
-	};
 
-	$scope.accounts = [306020, 306900, 310010, 310040, 310050, 312000, 313000, 313010, 313020, 320240, 320250, 321200, 322000, 322020, 322040, 325050, 325060, 325070, 326000, 329000, 329100, 330000];
+		$scope.alert = {
+			info: {
+				state: true,
+				value: $filter('translate')('reimbursement.expense.info')
+			},
+			success: {
+				state: false,
+				value: 'blabla'
+			},
+			danger: {
+				state: false,
+				value: 'blabla'
+			}
+		};
 
-	$scope.expense = {
-		id : 1,
-		creator : {
-			ldap_id : 23,
-			name : 'Jens Meier'
-		},
-		contact : {
-			person : {
-				ldap_id : 3,
-				name : 'Claudia Leibundgut'
+		// ToDo this needs to be sent from the server
+		$scope.expense = {
+			id: 1,
+			creator: {
+				ldap_id: 23,
+				name: 'Jens Meier'
 			},
-			phone : '044 786 23 45'
-		},
-		accounting : 'Reise nach New York',
-		note : [{
-			date : '2015-03-11T18:00:00.000+02:00',
-			person : {
-				ldap_id : 20,
-				name : 'Burkhard Stiller'
+			contact: {
+				person: {
+					ldap_id: 3,
+					name: 'Claudia Leibundgut'
+				},
+				phone: '044 786 23 45'
 			},
-			text : 'Es fehlen noch die Belege fÃ¼r das Abendessen im Fridays!'
-		}, {
-			date : '2015-03-12T16:30:00.000+02:00',
-			person : {
-				ldap_id : 23,
-				name : 'Jens Meier'
-			},
-			text : 'Habe die Belege hinzugefÃ¼gt'
-		}],
-		receipts : [{
-			id : 1,
-			date_receipt : '2015-01-13T18:00:00.000+02:00',
-			account : '310050',
-			description : 'Schwimmen im Hudson',
-			amount : '220.20',
-			cost_center : 'E-10000-01-01'
-		}, {
-			id : 2,
-			date_receipt : '2015-01-10T18:00:00.000+02:00',
-			account : '329100',
-			description : 'Flight ZRH => JFK',
-			amount : '1210.20',
-			cost_center : 'E-10000-01-01'
-		}, {
-			id : 3,
-			date_receipt : '2015-01-14T18:00:00.000+02:00',
-			account : '329100',
-			description : 'Flight JFK => ZRH',
-			amount : '1210.20',
-			cost_center : 'E-10000-01-01'
-		}, {
-			id : 4,
-			date_receipt : '2015-01-14T19:00:00.000+02:00',
-			account : '313020',
-			description : 'Zeichenblock',
-			amount : '10.20',
-			cost_center : 'E-10000-01-01'
-		}],
-		documents : [{
-			url : 'url_to_pdf_doc',
-			belongs_to_receipt : [2, 3]
-		}, {
-			url : 'url_to_pdf_doc',
-			belongs_to_receipt : [1]
-		}]
-	};
+			accounting: 'Reise nach New York',
+			note: [
+				{
+					date: '2015-03-11T18:00:00.000+02:00',
+					person: {
+						ldap_id: 20,
+						name: 'Burkhard Stiller'
+					},
+					text: 'Es fehlen noch die Belege für das Abendessen im Fridays!'
+				},
+				{
+					date: '2015-03-12T16:30:00.000+02:00',
+					person: {
+						ldap_id: 23,
+						name: 'Jens Meier'
+					},
+					text: 'Habe die Belege hinzugefügt'
+				}
+			],
+			receipts: [
+				{
+					id: 1,
+					date_receipt: '2015-01-13T18:00:00.000+02:00',
+					account: '310050',
+					description: 'Schwimmen im Hudson',
+					amount: {
+						amount_original: '150.75',
+						amount_currency: 'USD',
+						exchange_rate: '1.01',
+						amount: '155.80'
+					},
+					cost_center: 'E-10000-01-01'
+				},
+				{
+					id: 2,
+					date_receipt: '2015-01-10T18:00:00.000+02:00',
+					account: '329100',
+					description: 'Flight ZRH => JFK',
+					amount: {
+						amount_original: '150.75',
+						amount_currency: 'USD',
+						exchange_rate: '1.01',
+						amount: '155.80'
+					},
+					cost_center: 'E-10000-01-01'
+				},
+				{
+					id: 3,
+					date_receipt: '2015-01-14T18:00:00.000+02:00',
+					account: '329100',
+					description: 'Flight JFK => ZRH',
+					amount: {
+						amount_original: '150.75',
+						amount_currency: 'USD',
+						exchange_rate: '1.01',
+						amount: '155.80'
+					},
+					cost_center: 'E-10000-01-01'
+				},
+				{
+					id: 4,
+					date_receipt: '2015-01-14T19:00:00.000+02:00',
+					account: '313020',
+					description: 'Zeichenblock',
+					amount: {
+						amount_original: '150.75',
+						amount_currency: 'USD',
+						exchange_rate: '1.01',
+						amount: '155.80'
+					},
+					cost_center: 'E-10000-01-01'
+				}
+			],
+			documents: [
+				{
+					url: 'url_to_pdf_doc',
+					belongs_to_receipt: [2, 3]
+				},
+				{
+					url: 'url_to_pdf_doc',
+					belongs_to_receipt: [1]
+				}
+			]
+		};
 
-	$scope.documentExistsForExpense = function(receipt_id) {
-		for (var i = 0; i < $scope.expense.documents.length; i++) {
-			if ($scope.expense.documents[i].belongs_to_receipt.indexOf(receipt_id) !== -1) {
-				return true;
+		getTotal();
+
+		$scope.uploadReceipt = function () {
+			// ToDo
+		};
+
+		$scope.addNewReceipt = function () {
+			$scope.receipt = {
+				id: 0,
+				date_receipt: null,
+				account: null,
+				description: '',
+				amount: {
+					original: '',
+					currency: 'CHF',
+					exchange_rate: '1.00',
+					value: ''
+				},
+				cost_center: {
+					prefix: 'E-1000',
+					value: ''
+				}
+			};
+
+			var modalInstance = $modal.open({
+				templateUrl: 'expense/receipt.tpl.html',
+				controller: 'ReceiptController',
+				scope: $scope
+			});
+
+			modalInstance.result.then(function (response) {
+				console.log(response);
+			});
+		};
+
+		$scope.documentExistsForExpense = function (receipt_id) {
+			for (var i = 0; i < $scope.expense.documents.length; i++) {
+				if ($scope.expense.documents[i].belongs_to_receipt.indexOf(receipt_id) !== -1) {
+					return true;
+				}
+			}
+			return false;
+		};
+
+		$scope.expenseId = $stateParams.id;
+		$scope.receiptChanges = false;
+
+		$scope.$watch(function () {
+			console.log("digest called");
+		});
+
+		function find(obj, id) {
+			for (var i = 0; i < obj.length; i++) {
+				if (obj[i].id === id) {
+					return [i, obj[i]];
+				}
 			}
 		}
-		return false;
-	};
->>>>>>> 27b1a18acbaa4d5b77a4c153e28b4e329e94a047
 
-	$scope.deleteReceipt = function(receipt_id) {
-		var receipt = find($scope.expense.receipts, receipt_id);
-		var confirm = window.confirm($filter('translate')('reimbursement.expense.delete_text', {
-			name : receipt[1].description.substr(0, 25)
-		}));
+		$scope.alert = {
+			info: {
+				state: true,
+				value: $filter('translate')('reimbursement.expense.info')
+			},
+			success: {
+				state: false,
+				value: 'blabla'
+			},
+			danger: {
+				state: false,
+				value: 'blabla'
+			}
+		};
 
-<<<<<<< HEAD
-        if(confirm) {
-            $scope.receiptChanges = true;
-            $scope.expense.receipts.splice(receipt[0],1);
-            getTotal();
-        }
-    };
-=======
-		if (confirm) {
-			$scope.receiptChanges = true;
-			$scope.expense.receipts.splice(receipt[0], 1);
-		}
-	};
->>>>>>> 27b1a18acbaa4d5b77a4c153e28b4e329e94a047
+		$scope.accounts = [306020, 306900, 310010, 310040, 310050, 312000, 313000, 313010, 313020, 320240, 320250, 321200, 322000, 322020, 322040, 325050, 325060, 325070, 326000, 329000, 329100, 330000];
 
-	$scope.saveExpense = function(form) {
-		if (form.$dirty) {
-			$scope.alert.danger.state = true;
-			$scope.alert.danger.value = $filter('translate')('reimbursement.expense.dirty_form');
-		}
-	};
+		$scope.expense = {
+			id: 1,
+			creator: {
+				ldap_id: 23,
+				name: 'Jens Meier'
+			},
+			contact: {
+				person: {
+					ldap_id: 3,
+					name: 'Claudia Leibundgut'
+				},
+				phone: '044 786 23 45'
+			},
+			accounting: 'Reise nach New York',
+			note: [
+				{
+					date: '2015-03-11T18:00:00.000+02:00',
+					person: {
+						ldap_id: 20,
+						name: 'Burkhard Stiller'
+					},
+					text: 'Es fehlen noch die Belege fÃ¼r das Abendessen im Fridays!'
+				},
+				{
+					date: '2015-03-12T16:30:00.000+02:00',
+					person: {
+						ldap_id: 23,
+						name: 'Jens Meier'
+					},
+					text: 'Habe die Belege hinzugefÃ¼gt'
+				}
+			],
+			receipts: [
+				{
+					id: 1,
+					date_receipt: '2015-01-13T18:00:00.000+02:00',
+					account: '310050',
+					description: 'Schwimmen im Hudson',
+					amount: '220.20',
+					cost_center: 'E-10000-01-01'
+				},
+				{
+					id: 2,
+					date_receipt: '2015-01-10T18:00:00.000+02:00',
+					account: '329100',
+					description: 'Flight ZRH => JFK',
+					amount: '1210.20',
+					cost_center: 'E-10000-01-01'
+				},
+				{
+					id: 3,
+					date_receipt: '2015-01-14T18:00:00.000+02:00',
+					account: '329100',
+					description: 'Flight JFK => ZRH',
+					amount: '1210.20',
+					cost_center: 'E-10000-01-01'
+				},
+				{
+					id: 4,
+					date_receipt: '2015-01-14T19:00:00.000+02:00',
+					account: '313020',
+					description: 'Zeichenblock',
+					amount: '10.20',
+					cost_center: 'E-10000-01-01'
+				}
+			],
+			documents: [
+				{
+					url: 'url_to_pdf_doc',
+					belongs_to_receipt: [2, 3]
+				},
+				{
+					url: 'url_to_pdf_doc',
+					belongs_to_receipt: [1]
+				}
+			]
+		};
 
-	$scope.cancel = function(form) {
-		if (!form.$pristine || $scope.receiptChanges) {
-			var confirm = confirm($filter('transalte')('reimbursement.expense.confirm_cancel_unsaved_changes'));
+		$scope.documentExistsForExpense = function (receipt_id) {
+			for (var i = 0; i < $scope.expense.documents.length; i++) {
+				if ($scope.expense.documents[i].belongs_to_receipt.indexOf(receipt_id) !== -1) {
+					return true;
+				}
+			}
+			return false;
+		};
+
+		$scope.deleteReceipt = function (receipt_id) {
+			var receipt = find($scope.expense.receipts, receipt_id);
+			var confirm = window.confirm($filter('translate')('reimbursement.expense.delete_text', {
+				name: receipt[1].description.substr(0, 25)
+			}));
+
 
 			if (confirm) {
-				$state.go('dashboard');
+				$scope.receiptChanges = true;
+				$scope.expense.receipts.splice(receipt[0], 1);
+				getTotal();
 			}
-		}
-	};
+		};
 
-}]);
+		$scope.saveExpense = function (form) {
+			if (form.$dirty) {
+				$scope.alert.danger.state = true;
+				$scope.alert.danger.value = $filter('translate')('reimbursement.expense.dirty_form');
+			}
+		};
+
+		$scope.cancel = function (form) {
+			if (!form.$pristine || $scope.receiptChanges) {
+				var confirm = confirm($filter('transalte')('reimbursement.expense.confirm_cancel_unsaved_changes'));
+
+				if (confirm) {
+					$state.go('dashboard');
+				}
+			}
+		};
+
+	}]);
