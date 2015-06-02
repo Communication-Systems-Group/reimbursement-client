30024ec5ed4937f311ab189cc0a09587104a19ba
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2015-06-02 22:04:33 +0200

    fix login process

--- src/js/login/login.controller.js
+++ src/js/login/login.controller.js
@@ -1,27 +1,26 @@
-app.controller('LoginController', ['$rootScope','$scope', 'loginRestService','$state',
+app.controller('LoginController', ['$rootScope', '$scope', '$state', 'loginRestService', 'globalMessagesService',
 
-function($rootScope, $scope, loginRestService, $state) {
+function($rootScope, $scope, $state, loginRestService, globalMessagesService) {
 	"use strict";
 
 	$scope.form = {
-		username : null,
-		password : null
+		username: null,
+		password: null
 	};
 
-	$scope.loginSent = false;
-	$scope.loginSuccess = false;
 	$scope.submit = function() {
-			loginRestService.postLogin($scope.form).then(function() {
-			$scope.loginSuccess = true;
-		}, function() {
-		$scope.loginSuccess = false;
-		})['finally'](function() {
-			$scope.loginSent = true;
+		loginRestService.postLogin($scope.form).then(function() {
+
 			//make update the csrf Token
 			loginRestService.getUsername().then(function(result){
-				 $rootScope.username = result.data.uid.toUpperCase();
-				 $state.go('dashboard');
+				$rootScope.username = result.data.uid.toUpperCase();
+				$state.go('dashboard');
+			}, function() {
+				globalMessagesService.showGeneralError();
 			});
+
+		}, function() {
+			globalMessagesService.showError("reimbursement.globalMessage.loginError.title", "reimbursement.globalMessage.loginError.message");
 		});
 	};
 
