d52d081a606e2f8b48b3c4034608a5cdd354c2b4
Robin <mail@xorox.ch>
2015-07-06 10:55:04 +0200

    - Service call addded to create a pseudo expense

--- src/js/expense/expense.controller.js
+++ src/js/expense/expense.controller.js
@@ -1,10 +1,15 @@
 /**
  * Created by robinengbersen on 23.05.15.
  */
-app.controller('ExpenseController', ['$scope', '$filter', '$state', '$stateParams', '$modal',
+app.controller('ExpenseController', ['$scope', '$filter', '$state', '$stateParams', '$modal', 'expenseRestService', 'globalMessagesService',
 
-	function ($scope, $filter, $state, $stateParams, $modal) {
+	function ($scope, $filter, $state, $stateParams, $modal, expenseRestService, globalMessagesService) {
 		"use strict";
+
+		$scope.expenseId = $stateParams.id;
+		$scope.receiptChanges = false;
+
+		$scope.note = '';
 
 		$scope.find = function (obj, id) {
 			for (var i = 0; i < obj.length; i++) {
@@ -24,27 +29,6 @@ app.controller('ExpenseController', ['$scope', '$filter', '$state', '$stateParam
 			$scope.total = total;
 		};
 
-		// ToDo refactor with globalmessageservice
-		$scope.alert = {
-			info: {
-				state: true,
-				value: $filter('translate')('reimbursement.expense.info')
-			},
-			success: {
-				state: false,
-				value: 'blabla'
-			},
-			danger: {
-				state: false,
-				value: 'blabla'
-			}
-		};
-
-		$scope.expenseId = $stateParams.id;
-		$scope.receiptChanges = false;
-
-		$scope.note = '';
-
 		/**
 		 * Initalize empty expense object
 		 * @type {{id: number, creator: {name: string}, contact: {person: {name: string}, phone: string}, accounting: string, note: Array, receipts: Array}}
@@ -60,7 +44,7 @@ app.controller('ExpenseController', ['$scope', '$filter', '$state', '$stateParam
 				},
 				phone: ''
 			},
-			accounting: '',
+			bookingText: '',
 			note: [
 				{
 					date: '2015-05-11T18:00:00.000+02:00',
@@ -102,48 +86,55 @@ app.controller('ExpenseController', ['$scope', '$filter', '$state', '$stateParam
 		 */
 		$scope.addNewReceipt = function () {
 			var receipt_id = 1;
-			if ($scope.expense.receipts.length > 0) {
-				receipt_id = $scope.expense.receipts[($scope.expense.receipts.length - 1)].id + 1;
-			}
-			$scope.receipt = {
-				id: receipt_id,
-				uid: 0,
-				date_receipt: null,
-				account: null,
-				description: '',
-				amount: {
-					original: '',
-					currency: 'CHF',
-					exchange_rate: '1.00',
-					value: ''
-				},
-				cost_center: {
-					prefix: 'E-1000',
-					value: ''
-				},
-				isNew: true
-			};
 
-			$scope.modalReceipt = $modal.open({
-				templateUrl: 'expense/receipt.tpl.html',
-				controller: 'ReceiptController',
-				scope: $scope
-			});
+			if ($scope.expense.bookingText.length === 0) {
+				globalMessagesService.showWarning(
+					$filter('translate')('reimbursement.expense.dirty_form_title'),
+					$filter('translate')('reimbursement.expense.validation.bookingText'));
+			} else {
+				if ($scope.expense.receipts.length > 0) {
+					receipt_id = $scope.expense.receipts[($scope.expense.receipts.length - 1)].id + 1;
+				} else {
+					expenseRestService.getExpense('POST', {bookingText: 'jhgjh'})
+						.success(function (response) {
+							// store expense id created
+							console.log(response);
+						})
+						.error(function (response) {
+							console.log(response);
+						});
+				}
+				$scope.receipt = {
+					id: receipt_id,
+					uid: 0,
+					date_receipt: null,
+					account: null,
+					description: '',
+					amount: {
+						original: '',
+						currency: 'CHF',
+						exchange_rate: '1.00',
+						value: ''
+					},
+					cost_center: {
+						prefix: 'E-1000',
+						value: ''
+					},
+					isNew: true
+				};
+
+				$scope.modalReceipt = $modal.open({
+					templateUrl: 'expense/receipt.tpl.html',
+					controller: 'ReceiptController',
+					scope: $scope
+				});
+			}
 		};
 
-		$scope.alert = {
-			info: {
-				state: true,
-				value: $filter('translate')('reimbursement.expense.info')
-			},
-			success: {
-				state: false,
-				value: 'blabla'
-			},
-			danger: {
-				state: false,
-				value: 'blabla'
-			}
+		$scope.getCostCategories = function () {
+			expenseRestService.getCostCategories.then(function (result) {
+				$scope.costCategories = result;
+			});
 		};
 
 		$scope.accounts = [306020, 306900, 310010, 310040, 310050, 312000, 313000, 313010, 313020, 320240, 320250, 321200, 322000, 322020, 322040, 325050, 325060, 325070, 326000, 329000, 329100, 330000];
@@ -153,6 +144,12 @@ app.controller('ExpenseController', ['$scope', '$filter', '$state', '$stateParam
 			var confirm = window.confirm($filter('translate')('reimbursement.expense.delete_text', {
 				name: receipt[1].description.substr(0, 25)
 			}));
+
+			if (confirm) {
+				$scope.receiptChanges = true;
+				$scope.expense.receipts.splice(receipt[0], 1);
+				$scope.getTotal();
+			}
 		};
 
 		/**
@@ -179,8 +176,9 @@ app.controller('ExpenseController', ['$scope', '$filter', '$state', '$stateParam
 		 */
 		$scope.saveExpense = function (form) {
 			if (form.$dirty) {
-				$scope.alert.danger.state = true;
-				$scope.alert.danger.value = $filter('translate')('reimbursement.expense.dirty_form');
+				globalMessagesService.showWarning(
+					$filter('translate')('reimbursement.expense.dirty_form_title'),
+					$filter('translate')('reimbursement.expense.dirty_form'));
 			} else {
 				//ToDo save expense
 			}
--- src/js/expense/expense.restservice.js
+++ src/js/expense/expense.restservice.js
@@ -6,18 +6,37 @@ app.factory("expenseRestService", ['$http', 'HOST',
 	function ($http, HOST) {
 		"use strict";
 
-		return {
-			getExchangeRates: function (date) {
-				return $http.get(HOST + '/api/public/exchange-rate', {
-						params: {
-							date: date.toISOString().slice(0, 10)
-						}
+		function getExchangeRates(date) {
+			return $http.get(HOST + '/api/public/exchange-rate', {
+					params: {
+						date: date.toISOString().slice(0, 10)
 					}
-				)
-					;
-			}
+				}
+			);
 		}
-			;
 
-	}])
-;
+		function getCostCategories() {
+			return $http.get(HOST + '/api/user/costCategories');
+		}
+
+		/**
+		 *
+		 * @param method {POST, GET, PUT, DELETE}
+		 * @param data
+		 * @returns {HttpPromise}
+		 */
+		function getExpense(method, data) {
+			return $http({
+				method: method,
+				url: HOST + '/api/user/expenses',
+				data: data
+			});
+		}
+
+		return {
+			getExchangeRates: getExchangeRates,
+			getCostCategories: getCostCategories,
+			getExpense: getExpense
+		};
+
+	}]);
