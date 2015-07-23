cc66945103f517f493e0c889d07c939a9291adf8
Robin <mail@xorox.ch>
2015-07-23 10:07:41 +0200

    Added customized errorMessage for MissingCsrfTokenException on login.

--- src/js/login/login.controller.js
+++ src/js/login/login.controller.js
@@ -1,19 +1,33 @@
 app.controller('LoginController', ['$scope', '$state', 'loginRestService', 'globalMessagesService',
 
-function($scope, $state, loginRestService, globalMessagesService) {
-	"use strict";
+	function ($scope, $state, loginRestService, globalMessagesService) {
+		"use strict";
 
-	$scope.form = {
-		username: null,
-		password: null
-	};
+		$scope.form = {
+			username: null,
+			password: null
+		};
 
-	$scope.submit = function() {
-		loginRestService.postLogin($scope.form).then(function() {
-			window.location.reload();
-		}, function() {
-			globalMessagesService.showError("reimbursement.globalMessage.loginError.title", "reimbursement.globalMessage.loginError.message");
-		});
-	};
+		$scope.submit = function () {
+			loginRestService.postLogin($scope.form).then(function () {
+				window.location.reload();
+			}, function (response) {
+				console.log(response);
 
-}]);
+				var errorLabel;
+				switch(response.data.type) {
+					case 'MissingCsrfTokenException':
+						errorLabel = "reimbursement.globalMessage.loginError.MissingCsrfTokenException";
+						break;
+					case 'BadCredentialsException':
+						errorLabel = "reimbursement.globalMessage.loginError.BadCredentialsException";
+						break;
+					default:
+						errorLabel = "reimbursement.globalMessage.loginError.message";
+				}
+
+				globalMessagesService.showError("reimbursement.globalMessage.loginError.title", errorLabel);
+			});
+		};
+
+	}]);
