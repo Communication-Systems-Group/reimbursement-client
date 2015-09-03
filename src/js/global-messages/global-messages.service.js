4101dce9e1856dac7476e84c52498bc5feaa425a
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2015-09-03 23:21:50 +0200

    global messages: return promises & confirm messages

--- src/js/global-messages/global-messages.service.js
+++ src/js/global-messages/global-messages.service.js
@@ -1,62 +1,124 @@
-app.factory("globalMessagesService", ['$modal', '$translate',
+app.factory("globalMessagesService", ['$modal', '$translate', '$q',
 
-	function ($modal, $translate) {
+	function ($modal, $translate, $q) {
 		"use strict";
 
 		var openModalInstances = [];
 
+		// modal without confirmation (yes or no)
+		function show(type, title, message) {
+
+			var templateUrl;
+			if(type === 'error') {
+				templateUrl = 'global-messages/global-error.tpl.html';
+			}
+			else if(type === 'warning') {
+				templateUrl = 'global-messages/global-warning.tpl.html';
+			}
+			else if(type === 'info') {
+				templateUrl = 'global-messages/global-info.tpl.html';
+			}
+			else {
+				return;
+			}
+
+			var deferred = $q.defer();
+
+			$translate([title, message]).then(function (translations) {
+				var openModal = $modal.open({
+					templateUrl: templateUrl,
+					controller: 'GlobalMessagesController',
+					size: 'sm',
+					resolve: {
+						title: function () {
+							return translations[title];
+						},
+						message: function () {
+							return translations[message];
+						}
+					}
+				});
+
+				openModalInstances.push(openModal);
+				openModal.result.then(function() {
+					deferred.resolve();
+				}, function() {
+					deferred.reject();
+				});
+			});
+
+			return deferred.promise;
+		}
+
+		// modal with confirmation (yes or no)
+		function confirm(type, title, message, yes, no) {
+			yes = yes || "reimbursement.globalMessages.acceptButton";
+			no = no || "reimbursement.globalMessages.denyButton";
+
+			var templateUrl;
+			if(type === "error") {
+				templateUrl = 'global-messages/global-error-confirm.tpl.html';
+			}
+			else if(type === "warning") {
+				templateUrl = 'global-messages/global-warning-confirm.tpl.html';
+			}
+			else if(type === "info") {
+				templateUrl = 'global-messages/global-info-confirm.tpl.html';
+			}
+			else {
+				return;
+			}
+
+			var deferred = $q.defer();
+			$translate([title, message, yes, no]).then(function (translations) {
+				var openModal = $modal.open({
+					templateUrl: templateUrl,
+					controller: 'GlobalMessagesConfirmController',
+					size: 'sm',
+					resolve: {
+						title: function() {
+							return translations[title];
+						},
+						message: function() {
+							return translations[message];
+						},
+						yes: function() {
+							return translations[yes];
+						},
+						no: function() {
+							return translations[no];
+						}
+					}
+				});
+
+				openModalInstances.push(openModal);
+				openModal.result.then(function() {
+					deferred.resolve();
+				}, function() {
+					deferred.reject();
+				});
+			});
+
+			return deferred.promise;
+		}
+
 		function showError(title, message) {
-			$translate([title, message]).then(function (translations) {
-				openModalInstances.push($modal.open({
-					templateUrl: 'global-messages/global-error.tpl.html',
-					controller: 'GlobalMessagesController',
-					size: 'sm',
-					resolve: {
-						title: function () {
-							return translations[title];
-						},
-						message: function () {
-							return translations[message];
-						}
-					}
-				}));
-			});
+			return show('error', title, message);
 		}
-
 		function showWarning(title, message) {
-			$translate([title, message]).then(function (translations) {
-				openModalInstances.push($modal.open({
-					templateUrl: 'global-messages/global-warning.tpl.html',
-					controller: 'GlobalMessagesController',
-					size: 'sm',
-					resolve: {
-						title: function () {
-							return translations[title];
-						},
-						message: function () {
-							return translations[message];
-						}
-					}
-				}));
-			});
+			return show('warning', title, message);
 		}
-
 		function showInfo(title, message) {
-			$translate([title, message]).then(function (translations) {
-				openModalInstances.push($modal.open({
-					templateUrl: 'global-messages/global-info.tpl.html',
-					controller: 'GlobalMessagesController',
-					size: 'sm',
-					resolve: {
-						title: function () {
-							return translations[title];
-						},
-						message: function () {
-							return translations[message];
-						}
-					}
-				}));
-			});
+			return show('info', title, message);
+		}
+		function confirmError(title, message, yes, no) {
+			return confirm('error', title, message, yes, no);
+		}
+		function confirmWarning(title, message, yes, no) {
+			return confirm('warning', title, message, yes, no);
+		}
+		function confirmInfo(title, message, yes, no) {
+			return confirm('info', title, message, yes, no);
 		}
 
 		function hideMessages() {
@@ -85,15 +147,21 @@ app.factory("globalMessagesService", ['$modal', '$translate',
 		}
 
 		return {
+
 			showError: showError,
 			showWarning: showWarning,
 			showInfo: showInfo,
+			confirmError: confirmError,
+			confirmWarning: confirmWarning,
+			confirmInfo: confirmInfo,
+
 			hideMessages: hideMessages,
 			showGeneralError: function () {
 				showError("reimbursement.globalMessage.ServiceException.title",
 					"reimbursement.globalMessage.ServiceException.message");
 			},
 			isServerErrorMessages: isServerErrorMessages
+
 		};
 
 	}
