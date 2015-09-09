589a7136e2df97eca308b662812a3b8892e63796
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2015-09-09 17:11:01 +0200

    blubb

--- src/js/settings/settings.controller.js
+++ src/js/settings/settings.controller.js
@@ -1,15 +1,15 @@
-app.controller('SettingsController', ['$scope', 'settingsRestService', 'USER',
+app.controller('SettingsController', ['$scope', '$timeout', 'settingsRestService', 'USER',
 
-function($scope, settingsRestService, USER) {
+function($scope, $timeout, settingsRestService, USER) {
 	'use strict';
-	$scope.form={};
 	$scope.languages = [];
 	// $scope.selectedLanguage=USER.language;
 	USER.test=1;
 
 	settingsRestService.getSupportedLanguages().then(function(response){
-		$scope.languages=response.data;
-		$scope.form.selectedLanguage='EN';
-		console.log($scope.selectedLanguage);
+		$scope.languages = response.data;
+		$timeout(function() {
+			$scope.selectedLanguage = 'EN';
+		});
 	});
 }]);
