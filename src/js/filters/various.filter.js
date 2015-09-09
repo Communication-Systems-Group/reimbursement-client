bb60bb7c1ac4e50664d313ef7969e4346b70c6ca
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2015-09-09 10:56:29 +0200

    add description to administration

--- src/js/filters/various.filter.js
+++ src/js/filters/various.filter.js
@@ -1,7 +1,44 @@
-app.filter('costCategoryLanguage', [ '$translate', function ($translate) {
+app.filter('costCategoryLanguage', [ '$translate',
+
+function ($translate) {
 	'use strict';
 
 	return function (value) {
 		return value[$translate.use()];
 	};
+}]);
+
+app.filter('limitWordwise', [ '$translate', '$filter',
+
+function($translate, $filter) {
+	"use strict";
+
+	return function(input, limit) {
+		if(!input) {
+			return;
+		}
+		if(input.length <= limit) {
+			return input;
+		}
+		var afterLimitText = input.substr(limit);
+		if(afterLimitText === "") {
+			return $filter('limitTo')(input, limit);
+		}
+		else {
+			var spaceIndex = afterLimitText.indexOf(' ');
+			if(spaceIndex === -1) {
+				return $filter('limitTo')(input, limit);
+			}
+			else {
+				var calculatedLimit = limit + spaceIndex;
+				if(calculatedLimit === input.length) {
+					return $filter('limitTo')(input, calculatedLimit);
+				}
+				else {
+					return $filter('limitTo')(input, calculatedLimit) + ' (...)';
+				}
+			}
+		}
+	};
+
 }]);
