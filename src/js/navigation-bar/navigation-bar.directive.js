d9cdaf811ec8eb42427f32d3ef8f5d1ff51c37d9
Robin <mail@xorox.ch>
2015-09-21 16:58:23 +0200

    Added language switch for unauthorized user.

--- src/js/navigation-bar/navigation-bar.directive.js
+++ src/js/navigation-bar/navigation-bar.directive.js
@@ -1,7 +1,15 @@
-app.directive('navigationBar', ['USER',
+app.directive('navigationBar', ['USER', '$translate',
 
-function(USER) {
+function(USER, $translate) {
 	"use strict";
+
+    function reverselanguage(language) {
+        if(language === 'en') {
+            return 'de';
+        } else {
+            return 'en';
+        }
+    }
 
 	return {
 		restrict: 'E',
@@ -10,7 +18,13 @@ function(USER) {
 		link: function($scope) {
 
 			$scope.USER = USER;
+            $scope.inverseLanguage = reverselanguage($translate.use());
 
+            $scope.switchLanguage = function(language) {
+                $translate.use(language);
+
+                $scope.inverseLanguage = reverselanguage(language);
+            };
 		}
 	};
 
