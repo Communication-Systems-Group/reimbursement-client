092a6bc784d62afea2e81f40f1fdbcc9e8f74184
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2015-10-05 19:26:24 +0200

    remove unpleasent icons from state labels

--- src/js/expense/expense-state.directive.js
+++ src/js/expense/expense-state.directive.js
@@ -9,16 +9,14 @@ app.directive('expenseState',
 			scope: {
 				state: '='
 			},
-			templateUrl: 'expense/state-expense.tpl.html',
+			templateUrl: 'expense/expense-state.tpl.html',
 			link: function (scope) {
 				scope.states = {
 					'DRAFT': {
-						"icon": 'fa-file-o',
 						"class": 'label-info',
 						"translate": 'reimbursement.expense.state.draft'
 					},
 					'ASSIGNED_TO_PROF': {
-						"icon": 'fa-graduation-cap',
 						"class": 'label-info',
 						"translate": 'reimbursement.expense.state.assigned_to_professor'
 					},
@@ -27,37 +25,30 @@ app.directive('expenseState',
 						"translate": 'reimbursement.expense.state.to_be_assigned'
 					},
 					'ASSIGNED_TO_FINANCE_ADMIN': {
-						"icon": 'fa-money',
 						"class": 'label-info',
 						"translate": 'reimbursement.expense.state.assigned_to_finance_admin'
 					},
 					'REJECTED': {
-						"icon": 'fa-times-circle-o',
 						"class": 'label-danger',
 						"translate": 'reimbursement.expense.state.rejected'
 					},
 					'SIGNED': {
-						"icon": 'fa-check-square',
 						"class": 'label-success',
 						"translate": 'reimbursement.expense.state.signed'
 					},
 					'PRINTED': {
-						"icon": 'fa-print',
 						"class": 'label-success',
 						"translate": 'reimbursement.expense.state.printed'
 					},
 					'TO_SIGN_BY_USER': {
-						"icon": 'fa-pencil',
 						"class": 'label-info',
 						"translate": 'reimbursement.expense.state.toSignByUser'
 					},
 					'TO_SIGN_BY_PROF': {
-						"icon": 'fa-pencil',
 						"class": 'label-info',
 						"translate": 'reimbursement.expense.state.toSignByProf'
 					},
 					'TO_SIGN_BY_FINANCE_ADMIN': {
-						"icon": 'fa-pencil',
 						"class": 'label-info',
 						"translate": 'reimbursement.expense.state.toSignByFinanceAdmin'
 					}
