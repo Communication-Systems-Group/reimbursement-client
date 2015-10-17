78b63a1503ec0d553ebf0aec390aecffebbc74e4
davefromthecave <daveli-g@hotmail.com>
2015-10-17 20:08:02 +0200

    implemented paginnation in archive

--- src/js/archive/archive.controller.js
+++ src/js/archive/archive.controller.js
@@ -1,10 +1,34 @@
-app.controller('ArchiveController', ['$scope', 'archiveRestService',
+app.controller('ArchiveController', ['$scope', '$filter', 'archiveRestService',
 
-function($scope, archiveRestService) {
+function($scope, $filter, archiveRestService) {
 	'use strict';
 	$scope.archivedExpenses = [];
+	$scope.itemsPerPage = 15;
+	$scope.pagedExpenses = [];
+	$scope.currentPage = 1;
 
 	archiveRestService.getArchivedExpenses().then(function(response) {
 		$scope.archivedExpenses = response.data;
+		sortItems();
+		groupItemsToPages();
 	});
+
+	function sortItems() {
+		$scope.archivedExpenses = $filter('orderBy')($scope.archivedExpenses, 'date', true);
+	}
+
+	function groupItemsToPages() {
+		$scope.pagedExpenses = [];
+
+		for (var i = 0; i < $scope.archivedExpenses.length; i++) {
+			if (i % $scope.itemsPerPage === 0) {
+				$scope.pagedExpenses[Math.floor(i / $scope.itemsPerPage) + 1] = [$scope.archivedExpenses[i]];
+			} else {
+				$scope.pagedExpenses[Math.floor(i / $scope.itemsPerPage) + 1].push($scope.archivedExpenses[i]);
+			}
+		}
+
+		$scope.currentPage = 1;
+	}
+
 }]);
