8324b151a0841e5999cb8214e58df4ec86713b52
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2015-10-19 20:15:23 +0200

    beautify sign expense choose dialog

--- src/js/expense/sign-expense-form.controller.js
+++ src/js/expense/sign-expense-form.controller.js
@@ -3,6 +3,13 @@ app.controller('SignExpenseFormController', ['$scope', '$modalInstance',
 function($scope, $modalInstance) {
 	"use strict";
 
+	$scope.method = null;
+	$scope.dismiss = $modalInstance.dismiss;
+
+	$scope.selectMethod = function(method) {
+		$scope.method = method;
+	};
+
 	$scope.save = function() {
 		var hasDigitalSignature;
 		if ($scope.method === "digital") {
