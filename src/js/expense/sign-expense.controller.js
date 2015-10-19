8aac40e7999e95fdf04a8dd52be143f5a8510ca9
davefromthecave <daveli-g@hotmail.com>
2015-10-19 17:46:46 +0200

    implemented function to handle confirm message

--- src/js/expense/sign-expense.controller.js
+++ src/js/expense/sign-expense.controller.js
@@ -1,36 +1,35 @@
 app.controller('SignExpenseController', ['$scope', '$state', '$stateParams', '$uibModal', 'expenseRestService', 'globalMessagesService',
 
-	function ($scope, $state, $stateParams, $uibModal, expenseRestService, globalMessagesService) {
-		"use strict";
+function($scope, $state, $stateParams, $uibModal, expenseRestService, globalMessagesService) {
+	"use strict";
 
-		$scope.expenseUid = $stateParams.uid;
-		$scope.expenseItems = [];
-		$scope.expenseState = '';
+	$scope.expenseUid = $stateParams.uid;
+	$scope.expenseItems = [];
+	$scope.expenseState = '';
 
-		expenseRestService.getAccessRights($scope.expenseUid).then(function (response) {
+	expenseRestService.getAccessRights($scope.expenseUid).then(function(response) {
 
-			if (response.data.viewable && response.data.signable && !response.data.editable) {
-				expenseRestService.getExpense($scope.expenseUid).then(function (response) {
-					$scope.expenseAccountingText = response.data.accounting;
-					$scope.expenseState = response.data.state;
-				});
-			}
-			else {
-				$state.go('dashboard');
-			}
+		if (response.data.viewable && response.data.signable && !response.data.editable) {
+			expenseRestService.getExpense($scope.expenseUid).then(function(response) {
+				$scope.expenseAccountingText = response.data.accounting;
+				$scope.expenseState = response.data.state;
+			});
+		} else {
+			$state.go('dashboard');
+		}
 
-		}, function(response) {
-			// error handled in list-expense-items.directive
-			response.errorHandled = true;
-		});
+	}, function(response) {
+		// error handled in list-expense-items.directive
+		response.errorHandled = true;
+	});
 
-		expenseRestService.getExpense($scope.expenseUid).then(function (response) {
-			$scope.hasDigitalSignature = response.data.hasDigitalSignature;
-		});
+	expenseRestService.getExpense($scope.expenseUid).then(function(response) {
+		$scope.hasDigitalSignature = response.data.hasDigitalSignature;
+	});
 
-		$scope.sign = function () {
-			if($scope.expenseState === "TO_SIGN_BY_USER") {
-				var modalInstance = $uibModal.open({
+	$scope.sign = function() {
+		if ($scope.expenseState === "TO_SIGN_BY_USER") {
+			var modalInstance = $uibModal.open({
 				templateUrl: 'expense/sign-expense-form.tpl.html',
 				controller: 'SignExpenseFormController',
 				keyboard: false
@@ -40,15 +39,19 @@ app.controller('SignExpenseController', ['$scope', '$state', '$stateParams', '$u
 			modalInstance.result.then(function(response) {
 				$scope.hasDigitalSignature = response;
 				expenseRestService.setSignMethod($scope.expenseUid, $scope.hasDigitalSignature);
+				signElectronically();
 			});
-			}
+		} else {
+			signElectronically();
+		}
 
-			if($scope.hasDigitalSignature === false) {
+		function signElectronically() {
+			if ($scope.hasDigitalSignature === false) {
 				expenseRestService.signElectronically($scope.expenseUid).then(function() {
-					globalMessagesService.showInfoMd('reimbursement.expense.signInfoTitle',
-							'reimbursement.expense.signInfoMessage');
+					globalMessagesService.showInfoMd('reimbursement.expense.signInfoTitle', 'reimbursement.expense.signInfoMessage');
 				});
 			}
-		};
+		}
+	};
 
-	}]);
+}]);
