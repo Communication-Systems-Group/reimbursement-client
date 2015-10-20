2fe131dc2ee449689f4b431f828ddfa2de35c42f
davefromthecave <daveli-g@hotmail.com>
2015-10-21 00:16:23 +0200

    implemented data input for graphs

--- src/js/administration/admin-pool-graphs.controller.js
+++ src/js/administration/admin-pool-graphs.controller.js
@@ -1,41 +1,46 @@
-app.controller('AdminPoolGraphsController', ['$timeout', 'administrationRestService', 'c3',
+app.controller('AdminPoolGraphsController', ['$scope', '$timeout', 'administrationRestService', 'c3',
 
-function($timeout, administrationRestService, c3) {
+function($scope, $timeout, administrationRestService, c3) {
 	'use strict';
-	var data = [];
+	$scope.data = [];
+	$scope.modData = {
+		percentagePrinted: 0
+	};
 
 	administrationRestService.getExpenseStateRawData().then(function(response) {
-		data = response.data;
+		$scope.data = response.data;
 	});
 
 	$timeout(function() {
+
+		calculateValues();
+		console.log($scope.data);
+
 		c3.generate({
 			bindto: "#graph-donut-current-state-distribution",
 			data: {
-				type: 'donut',
+				type: 'bar',
 				columns: [
-					['DRAFT', 10],
-					['ASSIGNED_TO_PROF', 20],
-					['REJECTED', 30],
-					['TO_BE_ASSIGNED', 40],
-					['ASSIGNED_TO_FINANCE_ADMIN', 50],
-					['TO_SIGN_BY_USER', 60],
-					['TO_SIGN_BY_PROF', 70],
-					['TO_SIGN_BY_FINANCE_ADMIN', 80],
-					['SIGNED', 90]
-					//['PRINTED', 100]
+					['DRAFT', $scope.data.draft || 0],
+					['ASSIGNED_TO_PROF', $scope.data.assignedToProf || 0],
+					['REJECTED', $scope.data.rejected || 0],
+					['TO_BE_ASSIGNED', $scope.data.toBeAssigned || 0],
+					['ASSIGNED_TO_FINANCE_ADMIN', $scope.data.assignedToFinanceAdmin || 0],
+					['TO_SIGN_BY_USER', $scope.data.toSignByUser || 0],
+					['TO_SIGN_BY_PROF', $scope.data.toSignByProf || 0],
+					['TO_SIGN_BY_FINANCE_ADMIN', $scope.data.toSignByFianceAdmin || 0],
+					['SIGNED', $scope.data.signed || 0]
 				]
 			},
-			legend: { show: false }
+			legend: {
+				show: false
+			}
 		});
-
 		c3.generate({
 			bindto: "#graph-gauge-accepted-declined",
 			data: {
 				type: "gauge",
-				columns: [
-					['data', 61.4],
-				]
+				columns: [['Printed', $scope.modData.percentagePrinted || 0]]
 			},
 			color: {
 				pattern: ['#ff0000', '#f97600', '#f6c600', '#60b044'],
@@ -50,12 +55,8 @@ function($timeout, administrationRestService, c3) {
 			data: {
 				x: 'x',
 				type: 'area',
-				columns: [
-					['x', '2015-01-01', '2015-02-01', '2015-03-01', '2015-04-01', '2015-05-01', '2015-06-01', '2015-07-01', '2015-08-01', '2015-09-01', '2015-10-01', '2015-11-01', '2015-12-01'],
-					['finished', 5, 20, 40, 20, 40, 50, 70, 80, 101, 120, 140],
-					['not-yet-finished', 20, 30, 40, 20, 30, 80, 90, 40, 30, 20, 10]
-				],
-				groups: [[ 'finished', 'not-yet-finished' ]]
+				columns: [['x', '2015-01-01', '2015-02-01', '2015-03-01', '2015-04-01', '2015-05-01', '2015-06-01', '2015-07-01', '2015-08-01', '2015-09-01', '2015-10-01', '2015-11-01', '2015-12-01'], ['finished', 5, 20, 40, 20, 40, 50, 70, 80, 101, 120, 140], ['not-yet-finished', 20, 30, 40, 20, 30, 80, 90, 40, 30, 20, 10]],
+				groups: [['finished', 'not-yet-finished']]
 			},
 			axis: {
 				x: {
@@ -71,4 +72,11 @@ function($timeout, administrationRestService, c3) {
 			}
 		});
 	});
+
+	function calculateValues() {
+		if ($scope.data.printed !== 0) {
+			$scope.modData.percentagePrinted = ($scope.data.printed / $scope.data.totalAmountOfExpenses) * 100;
+		}
+	}
+
 }]);
