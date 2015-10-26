83bf24a4412418a2eeda2d14252a79d0df4a3b7f
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2015-10-26 14:36:43 +0100

    fix http interceptor

--- src/js/utility/http-interceptor.factory.js
+++ src/js/utility/http-interceptor.factory.js
@@ -2,7 +2,7 @@ app.factory('httpInterceptor', ['$q', '$timeout', '$injector',
 
 function ($q, $timeout, $injector) {
 	'use strict';
-	
+
 	function resetUserToPreLogin(){
 		var $state = $injector.get('$state');
 		if(!$state.is('logout') && !$state.is('login')) {
@@ -12,7 +12,6 @@ function ($q, $timeout, $injector) {
 			$uibModalStack.dismissAll();
 			USER.roles = [];
 			USER.loggedIn = false;
-			//TODO check this state.go?
 			$state.go('login');
 		}
 	}
@@ -22,18 +21,19 @@ function ($q, $timeout, $injector) {
 			if(response.status === 401) {
 				resetUserToPreLogin();
 				return $q.reject(response);
+
 			} else if(response.status === 403) {
 				// The 403 error message can be suppressed if, immediately inside the error function,
-				// response.errorHandled is set to true. This will stop the global error handler to open a modal.
+				// response.csrfErrorHandled is set to true. This will stop the global error handler to open a modal.
 				return $q.reject(response)['finally'](function() {
 					$timeout(function() {
 						if(response.csrfErrorHandled !== true) {
 							resetUserToPreLogin();
+							$injector.get('loginRestService').getUsername();
+
 							var globalMessagesService = $injector.get('globalMessagesService');
 							globalMessagesService.showErrorMd("reimbursement.globalMessage.InvalidCsrfTokenException.title",
-							"reimbursement.globalMessage.InvalidCsrfTokenException.message").then()['finally'](function() {
-								$injector.get('loginRestService').getUsername();
-							});
+								"reimbursement.globalMessage.InvalidCsrfTokenException.message");
 						}
 					});
 				});
