c2985ab01cc476e137997fb8f1b3576721a560a7
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2015-11-27 20:52:30 +0100

    fix progress indicator after printing an expense

--- src/js/expense/progress-indicator.directive.js
+++ src/js/expense/progress-indicator.directive.js
@@ -11,40 +11,44 @@ function(stateService) {
 			state: '='
 		},
 		link: function($scope) {
-			var stateList = stateService.getStateListWorkflow();
-
 			$scope.states = [];
-			var activeStatePassedBy = false;
-			for(var i = 0; i < stateList.length; i++) {
-				var currentClass = "";
-				if(stateList[i] === 'DRAFT' && $scope.state === 'REJECTED') {
-					activeStatePassedBy = true;
-					$scope.states.push({
-						"class": 'active rejected',
-						"state": $scope.state,
-						"first": i === 0
-					});
-				}
-				else {
-					if(stateList[i] === $scope.state) {
+
+			$scope.$watch('state', function(state) {
+				var stateList = stateService.getStateListWorkflow();
+
+				$scope.states = [];
+				var activeStatePassedBy = false;
+				for(var i = 0; i < stateList.length; i++) {
+					var currentClass = "";
+					if(stateList[i] === 'DRAFT' && state === 'REJECTED') {
 						activeStatePassedBy = true;
-						currentClass = "active";
+						$scope.states.push({
+							"class": 'active rejected',
+							"state": state,
+							"first": i === 0
+						});
 					}
 					else {
-						if(activeStatePassedBy) {
-							currentClass = "afterActive";
+						if(stateList[i] === state) {
+							activeStatePassedBy = true;
+							currentClass = "active";
 						}
 						else {
-							currentClass = "beforeActive";
+							if(activeStatePassedBy) {
+								currentClass = "afterActive";
+							}
+							else {
+								currentClass = "beforeActive";
+							}
 						}
+						$scope.states.push({
+							"class": currentClass,
+							"state": stateList[i],
+							"first": i === 0
+						});
 					}
-					$scope.states.push({
-						"class": currentClass,
-						"state": stateList[i],
-						"first": i === 0
-					});
 				}
-			}
+			});
 		}
 	};
 }]);
